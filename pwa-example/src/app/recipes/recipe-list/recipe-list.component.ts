970c6ae080b45c2076702b868879fbeae4204d45
Bradley Guthrie <guppy96@gmail.com>
2019-10-05 19:51:42 -0500

    recipe card built

--- pwa-example/src/app/recipes/recipe-list/recipe-list.component.ts
+++ pwa-example/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: [];
+  recipes: Recipe[] = [
+    new Recipe('a test recipe', 'this is a test', 'https://www.maxpixel.net/static/photo/2x/Food-Vegetable-White-Kidney-Bean-Recipe-2728708.jpg')
+  ];
 
   constructor() { }
 
