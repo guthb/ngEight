e3587b6b70217dee4380e311d40d9386cba666a0
Bradley Guthrie <guppy96@gmail.com>
2019-10-12 12:29:45 -0500

    wrong syntax was setting property instead of function and could not have empty funtions

--- pwa-example/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ pwa-example/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,6 +10,9 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  // ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
@@ -15,7 +20,10 @@ export class ShoppingEditComponent implements OnInit {
   }
 
   onAddItem() {
-
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 
