671dce916708abbbb1b7f26fe20ff8337cb83c2b
Bradley Guthrie <guppy96@gmail.com>
2019-10-14 19:47:31 -0500

    two serivce shells created through cli

--- pwa-example/src/app/shared/dropdown.directive.ts
+++ pwa-example/src/app/shared/dropdown.directive.ts
@@ -8,5 +8,10 @@ export class DropdownDirective {
 
     @HostListener('click') toggleOpen() {
         this.isOpen = !this.isOpen;
+
+        // @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        //     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+
     }
+    //constructor(private elRef: ElementRef) {}
 }
