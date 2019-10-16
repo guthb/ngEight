f4062f81027d68b26df41abf1fe7febab57520a8
Bradley Guthrie <guppy96@gmail.com>
2019-10-15 19:57:32 -0500

    service added to pick up selected recipe

--- pwa-example/src/app/recipes/recipe.service.ts
+++ pwa-example/src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+
   private recipes: Recipe[] = [
     new Recipe('a test recipe', 'this is a test', 'https://www.maxpixel.net/static/photo/2x/Food-Vegetable-White-Kidney-Bean-Recipe-2728708.jpg'),
 
