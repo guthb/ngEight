380471e5cd50a0c1632fc856ebae170f86188fb1
Bradley Guthrie <guppy96@gmail.com>
2019-10-26 15:38:51 -0500

    enhancing edit service

--- pwa-example/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ pwa-example/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model'
 import { ShoppingListService } from '../shopping-list.service';
-import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subcription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+
   constructor(private shoppinglistService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subcription = this.shoppinglistService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+        });
   }
 
   onAddItem(form: NgForm) {
@@ -21,5 +33,8 @@ export class ShoppingEditComponent implements OnInit {
     this.shoppinglistService.addIngredient(newIngredient);
   }
 
+  ngOnDestroy() {
+    this.subcription.unsubscribe();
+  }
 
 }
