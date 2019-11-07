e9d6fe487109a4c55bc863ecb0bfa8cd5e6e82f2
Bradley Guthrie <guppy96@gmail.com>
2019-11-06 18:59:52 -0600

    server name and content now being populated with custom event listener

--- databinding/src/app/cockpit/cockpit.component.ts
+++ databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-cockpit',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-
-  snewServerName = '';
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -16,19 +18,12 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.blueprintCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+
   }
 
 
