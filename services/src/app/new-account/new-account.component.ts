204ba6b5370d1622fde42beaa2316798291abb12
Bradley Guthrie <guppy96@gmail.com>
2019-11-21 17:38:15 -0600

    service added into two different components

--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,13 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-//import { LoggingService } from '../logging.service'  this is the incorrect manual way
+import { LoggingService } from '../logging.service';  //this is the incorrect manual way
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+
+  constructor(private loggingService: LoggingService) { }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -18,5 +21,6 @@ export class NewAccountComponent {
     // const service = new LoggingService();
     // service.logStatusChange(accountStatus)
     //console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
