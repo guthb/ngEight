61fc4394909dcd2206281272dfc4b27d22017392
Bradley Guthrie <guppy96@gmail.com>
2019-11-21 17:14:10 -0600

    incorrect way to add service

--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+//import { LoggingService } from '../logging.service'  this is the incorrect manual way
 
 @Component({
   selector: 'app-new-account',
@@ -6,13 +7,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    // this is the incorrect way to manually build service
+    // const service = new LoggingService();
+    // service.logStatusChange(accountStatus)
+    //console.log('A server status changed, new status: ' + accountStatus);
   }
 }
