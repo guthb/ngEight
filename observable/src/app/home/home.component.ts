cf53f8da560ecc017cf9982d8169f3a154d7bcac
Bradley Guthrie <guppy96@gmail.com>
2019-12-05 18:36:29 -0600

    created memory leak with observables

--- observable/src/app/home/home.component.ts
+++ observable/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    interval(1000).subscribe(count => {
+      console.log(count);
+    });
   }
 
 }
