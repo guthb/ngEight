a9b56c5d09aac6e54a3fc9295e0c2882e353edd2
Bradley Guthrie <guppy96@gmail.com>
2019-12-05 18:44:09 -0600

    created subscrition for observable so that i can destroy the subscripton to that observable when i change componenets

--- observable/src/app/home/home.component.ts
+++ observable/src/app/home/home.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObservableSubcription: Subscription
 
   constructor() { }
 
   ngOnInit() {
-    interval(1000).subscribe(count => {
+    //interval(1000).subscribe(count => {
+    this.firstObservableSubcription = interval(1000).subscribe(count => {
       console.log(count);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObservableSubcription.unsubscribe();
   }
 
 }
