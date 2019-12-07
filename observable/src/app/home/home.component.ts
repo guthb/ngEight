c0e80e738310d82ab3c81e417101b04d28a810a8
Bradley Guthrie <guppy96@gmail.com>
2019-12-07 08:45:24 -0600

    error handling and completion handling example

--- observable/src/app/home/home.component.ts
+++ observable/src/app/home/home.component.ts
@@ -21,15 +21,28 @@ export class HomeComponent implements OnInit, OnDestroy {
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
-
         observer.next(count);
+        //will count up to two and just stop
+        if (count === 2) {
+          observer.complete();
+        }
+        ///if error with observable it just dies, cancels it, nothing else happens
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
         count++;
       }, 1000)
     });
 
-    customIntervalObservable.subscribe(data => {
+    this.firstObservableSubcription = customIntervalObservable.subscribe(data => {
       console.log(data);
-    })
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      //handling a complete reaction, the completion handler does not fire with error
+      console.log('Completed!');
+    });
   }
 
   ngOnDestroy(): void {
