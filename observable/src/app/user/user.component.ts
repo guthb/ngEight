20e0e55efe449ccbcab37327e4822cb5629fd742
Bradley Guthrie <guppy96@gmail.com>
2019-12-08 11:23:11 -0600

    issue was onclick instead of click

--- observable/src/app/user/user.component.ts
+++ observable/src/app/user/user.component.ts
@@ -20,7 +20,7 @@ export class UserComponent implements OnInit {
   }
 
   onActivate() {
-    this.userService.activatedEmitter.emit(true)
+    this.userService.activatedEmitter.emit(true);
   }
 
 }
