52e4213a71e60031ca85fe1c1eefabef39ec19d5
Bradley Guthrie <guppy96@gmail.com>
2019-12-15 15:20:31 -0600

    added validation to display answers if form is valid

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -11,11 +11,23 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.signupForm.setValue({ userData: { username: suggestedName, email: '' }, secret: 'pet', questionAnswer: '', gender: 'male' });
-
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -25,6 +37,12 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.userData.secret;
+    this.user.answer = this.signupForm.value.userData.questionAnswer;
+    this.user.gender = this.signupForm.value.userData.gender;
   }
 
 
