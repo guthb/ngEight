f047e8ad792c1d5a091a09c8adad394a0d32e158
Bradley Guthrie <guppy96@gmail.com>
2019-12-20 19:33:41 -0600

    nested form conrol does not work with template

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -17,8 +17,11 @@ export class AppComponent {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+
       'gender': new FormControl('male')
     });
   }
