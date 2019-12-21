25e7e0e9c88091e1f00621230a90b264566464fb
Bradley Guthrie <guppy96@gmail.com>
2019-12-21 14:03:01 -0600

    custom name validator that does not allow two usernames

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -10,13 +10,15 @@ export class AppComponent {
 
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Teddy', "Mona"];
+
 
   ngOnInit() {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -31,6 +33,14 @@ export class AppComponent {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
+
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidde': true };
+    }
+    return null;
   }
 
 
