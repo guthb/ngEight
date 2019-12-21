31ac1d89546695307f336445955d6abba135e812
Bradley Guthrie <guppy96@gmail.com>
2019-12-21 13:42:47 -0600

    found the issue with the form group property binding

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +11,6 @@ export class AppComponent {
   genders = ['male', 'female'];
   signupForm: FormGroup;
 
-
-
   ngOnInit() {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -21,13 +19,18 @@ export class AppComponent {
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
-
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm)
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control)
   }
 
 
