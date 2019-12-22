9585b7cbbee3ec5b0da0d6f309465f1d48e24974
Bradley Guthrie <guppy96@gmail.com>
2019-12-22 13:06:24 -0600

    validator firing, but not displaying error to dom

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,8 @@ export class AppComponent {
   genders = ['male', 'female'];
   signupForm: FormGroup;
   forbiddenUsernames = ['Teddy', "Mona"];
+
+  constructor(private formBuilder: FormBuilder) { }
 
 
   ngOnInit() {
@@ -34,7 +36,6 @@ export class AppComponent {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control)
   }
-
 
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
