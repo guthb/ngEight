042e17862926968e6d30c745e9d4d149879729f8
Bradley Guthrie <guppy96@gmail.com>
2019-12-29 16:43:12 -0600

    firebase set back up and post sent successfully

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -15,11 +15,9 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    console.log(postData);
     this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
+      .post("https://http-ng8-lab-a9882.firebaseio.com/posts.json", postData)
       .subscribe(responseData => {
         console.log(responseData);
       });
