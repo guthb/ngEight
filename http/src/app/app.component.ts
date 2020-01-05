c587c8d2fabec34a3e7950c4930bb5f604675e63
Bradley Guthrie <guppy96@gmail.com>
2020-01-05 09:53:32 -0600

    adding error handeling

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) { }
 
@@ -20,7 +21,11 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
-    });
+    },
+      error => {
+        this.error = error.message;
+      }
+    );
   }
 
   onCreatePost(postData: Post) {
@@ -43,6 +48,9 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
+      console.log('error response', error)
     });
   }
 
