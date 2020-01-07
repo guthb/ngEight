6b407b689baab94b8367c1feba45f4a39fe42b3f
Bradley Guthrie <guppy96@gmail.com>
2020-01-06 19:33:05 -0600

    adding subscription based subject for errors

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit OnDestroy {
   loadedPosts = [];
   isFetching = false;
   error = null;
-
+  private errorSub: Subscription;
   constructor(private http: HttpClient, private postsService: PostsService) { }
 
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
     this.isFetching = true;
     this.postsService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
@@ -50,7 +55,7 @@ export class AppComponent implements OnInit {
       this.loadedPosts = posts;
     }, error => {
       this.error = error.message;
-      console.log('error response', error)
+      console.log('error response', error);
     });
   }
 
@@ -59,5 +64,12 @@ export class AppComponent implements OnInit {
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.errorSub.unsubscribe();
+
   }
 }
