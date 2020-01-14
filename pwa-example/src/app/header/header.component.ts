a8e48c7bc44f79ea91bb9ba7158ced696638436c
Bradley Guthrie <guppy96@gmail.com>
2020-01-13 19:38:45 -0600

    added resolver to enable the bringing back of data that is saved

--- pwa-example/src/app/header/header.component.ts
+++ pwa-example/src/app/header/header.component.ts
@@ -1,21 +1,18 @@
-import { Component } from '@angular/core';
-import { DataStorageService } from '../shared/data-storage.service';
+import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService) {}
 
   onSaveData() {
     this.dataStorageService.storeReciepes();
-
   }
 
   onFechData() {
-    this.dataStorageService.fetchRecipes();
+    this.dataStorageService.fetchRecipes().subscribe();
   }
-
-
 }
