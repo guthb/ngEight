7acd07fb54be5ccf5d17dd29ec79930262661497
Bradley Guthrie <guppy96@gmail.com>
2020-01-20 10:17:08 -0600

    adding better error logging to handle firebase codes

--- pwa-example/src/app/auth/auth.service.ts
+++ pwa-example/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs/operators';
 
 
 interface AuthResponseData {
@@ -18,7 +20,7 @@ export class AuthService {
 
   signup(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'enter url here'
+      'enter url here',
       {
         // tslint:disable-next-line: object-literal-shorthand
         email: email,
@@ -26,6 +28,30 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    );
+    ).pipe(catchError(errorResponse => {
+      let errorMessage = 'An unknown error occured!';
+      if (!errorResponse.error || !errorResponse.error.error) {
+        return throwError(errorMessage);
+      }
+      switch (errorResponse.error.message) {
+        case 'EMAIL_EXISTS': {
+          errorMessage = 'this email exists already';
+          break;
+        }
+        case 'OPERATION_NOT_ALLOWED': {
+          errorMessage = 'Password sign-in is disabled for this project';
+          break;
+        }
+        case 'EMAIL_EXISTS': {
+          errorMessage = 'All requests blocked from this device due to unusual activity. Try again later';
+          break;
+        }
+        default: {
+          errorMessage = errorMessage;
+          break;
+        }
+      }
+      return throwError(errorMessage);
+    }));
   }
 }
