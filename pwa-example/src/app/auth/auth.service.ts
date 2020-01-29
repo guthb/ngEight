5467359e0ff66eaa8283ceedf54213a07ad2ee06
Bradley Guthrie <guppy96@gmail.com>
2020-01-29 17:03:01 -0600

    auto logout now implemented

--- pwa-example/src/app/auth/auth.service.ts
+++ pwa-example/src/app/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { throwError, Subject, BehaviorSubject } from "rxjs";
 import { Config } from '../config';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { DataStorageService } from '../shared/data-storage.service';
 
 //const config = require('../config.ts') as Config;
 
@@ -23,6 +24,7 @@ export class AuthService {
   // myKey = config.myKey;
   url = new Config
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -72,21 +74,34 @@ export class AuthService {
 
     if (loadedUser.token) {
       this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+      this.autoLogout(expirationDuration);
     }
 
   }
-
 
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth'])
     localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer)
+    }
+    this.tokenExpirationTimer = null
   }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration); //2000
+  }
+
 
   private handleAuth(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user)
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
