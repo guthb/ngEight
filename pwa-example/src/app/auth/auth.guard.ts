acd6962e74e12c38975d2064a27406cc2ce7662d
Bradley Guthrie <guppy96@gmail.com>
2020-01-30 18:03:30 -0600

    using tap to add to route to /auth

--- pwa-example/src/app/auth/auth.guard.ts
+++ pwa-example/src/app/auth/auth.guard.ts
@@ -1,12 +1,12 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service'
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -14,11 +14,13 @@ export class AuthGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     return this.authService.user.pipe(map(user => {
       return !!user;
-    }));
+    }),
+      tap(isAuth => {
+        if (!isAuth) {
+          this.router.navigate(['/auth'])
+        }
+      }));
   }
-
-
-
 }
 
 
