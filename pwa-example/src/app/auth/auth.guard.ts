09f40185aa4540b202f45d99760c1776b38bc31c
Bradley Guthrie <guppy96@gmail.com>
2020-01-30 18:10:27 -0600

    only just watching the guard at use

--- pwa-example/src/app/auth/auth.guard.ts
+++ pwa-example/src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTr
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service'
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take } from 'rxjs/operators';
 import { Url } from 'url';
 
 @Injectable({ providedIn: 'root' })
@@ -13,14 +13,16 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot
   ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
-    return this.authService.user.pipe(map(user => {
-      const isAuth = !!user;
-      if (isAuth) {
-        return true;
-      }
-      return this.router.createUrlTree(['/auth'])
-      // return !!user;
-    }),
+    return this.authService.user.pipe(
+      take(1),
+      map(user => {
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
+        }
+        return this.router.createUrlTree(['/auth'])
+        // return !!user;
+      }),
       // tap(isAuth => {
       //   if (!isAuth) {
       //     this.router.navigate(['/auth'])
