a7435cd0c7cf1d48b2504938c422845758482f17
Bradley Guthrie <guppy96@gmail.com>
2020-02-13 18:42:07 -0600

    reducer component complete

--- pwa-example/src/app/shopping-list/shopping-list.actions.ts
+++ pwa-example/src/app/shopping-list/shopping-list.actions.ts
@@ -1,6 +1,7 @@
-export const ADD_INGREDIENT = 'ADD_INGREDIENT'
 import { Action } from '@ngrx/store';
 import { Ingredient } from '../shared/ingredient.model';
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT'
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
