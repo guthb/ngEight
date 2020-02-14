22b3551610454d3745c2f31ee399530e50bf057b
Bradley Guthrie <guppy96@gmail.com>
2020-02-13 19:06:18 -0600

    app now sees reducer

--- pwa-example/src/app/app.module.ts
+++ pwa-example/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store'
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { LoggingService } from './logging.service';
+import { shoppingListReducer } from './shopping-list/shopping-list.reducer';
+
 
 
 @NgModule({
@@ -22,11 +24,13 @@ import { LoggingService } from './logging.service';
     AppRoutingModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
     SharedModule,
     CoreModule
+
   ],
   bootstrap: [AppComponent]
-  // providers: [LoggingService]
+
 
 })
 export class AppModule { }
