f31b073b00a3d21fe32be2e4d82d79d21c12c6c9
Bradley Guthrie <guppy96@gmail.com>
2020-03-06 19:10:28 -0600

    able to edit recipes now but on reload state is not kept

--- pwa-example/src/app/recipes/recipes-resolver.service.ts
+++ pwa-example/src/app/recipes/recipes-resolver.service.ts
@@ -4,25 +4,38 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot
 } from "@angular/router";
+import { Store } from '@ngrx/store';
+import { Actions, ofType } from '@ngrx/effects';
+
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
 import { RecipeService } from "./recipe.service";
+import * as fromApp from '../store/app.reducer';
+import * as RecipeActions from '../recipes/store/recipe.actions';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: "root"
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(
-    private dataStorageService: DataStorageService,
-    private recipiesService: RecipeService
-  ) {}
+    private store: Store<fromApp.AppState>,
+    private actions$: Actions
+  ) { }
+
+
+  //  private dataStorageService: DataStorageService
+  //  private recipiesService: RecipeService
+
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const recipes = this.recipiesService.getRecipes();
-    if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
-    } else {
-      return recipes;
-    }
+    //const recipes = this.recipiesService.getRecipes();
+    //if (recipes.length === 0) {
+    //return this.dataStorageService.fetchRecipes();
+    //} else {
+    //  return recipes;
+    //}
+    this.store.dispatch(new RecipeActions.FetchRecipes());
+    return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1))
   }
 }
