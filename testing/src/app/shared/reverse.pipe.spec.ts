469ee5846adf781919ae4e9ff2e37db12b6d4f4d
Bradley Guthrie <guppy96@gmail.com>
2020-03-25 19:53:50 -0500

    several examples of tests

--- testing/src/app/shared/reverse.pipe.spec.ts
+++ testing/src/app/shared/reverse.pipe.spec.ts
@@ -1,10 +1,12 @@
 /* tslint:disable:no-unused-variable */
 
 import { ReversePipe } from "./reverse.pipe";
+
 describe('Pipe: ReversePipe', () => {
   it('should reverse the inputs', () => {
     let reversePipe = new ReversePipe();
-    expect(reversePipe.transform('hello')).toEqual('olleh');
+    expect(reversePipe.transform('happy')).toEqual('yppah');
   });
 
 });
+//isolated test
